d7f43e1260a0174f380db5ed3da6996bba167766
thfeng <tianhaofeng726@sina.com>
2022-12-18 00:24:31 +0800

    update route

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { IntlProvider } from 'react-intl';
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Link, Navigate, Route, Routes } from 'react-router-dom';
 import Home from './pages/home/Home';
 import About from './pages/about/About';
 import Messages from '@/locales/zh-CN.json';
@@ -16,10 +16,9 @@ const App = () => {
           <Link to="/about">View About</Link>
         </nav>
         <Routes>
-          <Route path="/" element={<Home></Home>}></Route>
-        </Routes>
-        <Routes>
+          <Route path="/home" element={<Home></Home>}></Route>
           <Route path="/about" element={<About></About>}></Route>
+          <Route path="/" element={<Navigate replace to="/home" />}></Route>
         </Routes>
       </BrowserRouter>
     </IntlProvider>
